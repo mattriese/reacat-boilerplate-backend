51996db41a8e0404c1cbba0a9b642c25e09444d8
Matt Riese <riese.matt@gmail.com>
2021-10-22 17:11:20 -0700

    added comments

--- routes/quotes.js
+++ routes/quotes.js
@@ -9,13 +9,16 @@ const quotes = [
   'This is heavy, Doc.',
   "There's a rythmic ceremonial ritual coming up!",
 ];
-
+/** GET request route returns list of all quotes */
 router.get('/', function sendQuotes(req, res) {
+  // If this was a real database query, this would be wrapped in a try/catch
   return res.json({ quotes });
 });
 
+/** POST request route takes a string and returns that string after prepending it to quotes array. */
 router.post('/', function updateQuotes(req, res) {
   const newQuote = req.body.newQuote;
+  // If this was a real database query, this would be wrapped in a try/catch
   quotes.unshift(newQuote);
   console.log('POST req.body ===', req.body);
   // console.log('PATCH req ===', req);
